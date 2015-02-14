c6d618fbf524e5c2fd9226f281aaf02ca27d9018
yukiboard <yuki@yukiboard.tk>
2015-02-14 19:24:20 +0900

    Cookie Support 1

---
+++ EpgTimerWeb2/WebServer/Cookie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpgTimer
+{
+    public class Cookie
+    {
+        private static string GetExpireHeader(DateTime Time)
+        {
+            return Time.ToString("R");
+        }
+
+    }
+}
