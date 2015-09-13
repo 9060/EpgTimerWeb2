a90f54280eacd90d7eefa991ac4940fcdb674864
yukiboard <yuki@yukiboard.com>
2015-09-13 17:19:12 +0900

    EpgPageの修正
    Signed-off-by: yukiboard <yuki@yukiboard.com>

--- EpgTimerWeb2/WebContent/EpgPage.cs
+++ EpgTimerWeb2/WebContent/EpgPage.cs
@@ -27,9 +27,15 @@ namespace EpgTimer
 {
     public class EpgPage
     {
+        private static DateTime ConvertDateTime(DateTime date)
+        {
+            return date
+                .AddTicks(-(date.Ticks % TimeSpan.TicksPerSecond))
+                .AddSeconds(-(UnixTime.ToUnixTime(date) % 3600));
+        }
         public static string Generate(DateTime StartTime, int MaxHour, List<ulong> ServiceKeys = null, int MinSize = 5, bool SetBgColor = true, bool IncludeNotEpg = false, bool EpgCapOnly = true, EpgSearchKeyInfo Search = null)
         {
-            DateTime Start = StartTime.AddSeconds(StartTime.Second * -1).AddMinutes(StartTime.Minute * -1);
+            DateTime Start = ConvertDateTime(StartTime.AddSeconds(StartTime.Second * -1).AddMinutes(StartTime.Minute * -1));
             DateTime End = Start.AddHours(MaxHour);
             StringBuilder sb = new StringBuilder();
             StringBuilder sb1 = new StringBuilder();
@@ -118,22 +124,21 @@ namespace EpgTimer
                 }
             }
             
-            SortedList<DateTime, DateTime> TimeList = new SortedList<DateTime, DateTime>(); ;
+            SortedList<long, DateTime> TimeList = new SortedList<long, DateTime>();
             foreach (var Item3 in Out.Values)
             {
                 foreach (var Item2 in Item3)
                 {
-                    var TempDate = Item2.Start;
-                    var TempDate2 = Item2.End;
+                    var TempDate = ConvertDateTime(Item2.Start);
+                    var TempDate2 = ConvertDateTime(Item2.End);
                     if (TempDate < Start)
                         TempDate = Start;
                     if (TempDate2 > End)
                         TempDate2 = End;
-                    if (UnixTime.ToUnixTime(TempDate) % 3600 != 0)
-                        TempDate = TempDate.AddSeconds(-1 * (UnixTime.ToUnixTime(TempDate) % 3600));
                     for (; TempDate < TempDate2; )
                     {
-                        TimeList[TempDate] = TempDate;
+                        long Unix = UnixTime.ToUnixTime(TempDate);
+                        TimeList[Unix] = TempDate;
                         TempDate = TempDate.AddHours(1);
                     }
                 }
@@ -154,7 +159,7 @@ namespace EpgTimer
             foreach (var Item in Out)
             {
                 StringBuilder sb2 = new StringBuilder();
-                sb2.AppendFormat("<div class=\"list\" data-t=\"{0}\" data-o=\"{1}\" data-s=\"{2}\">", Item.Key.TSID, Item.Key.ONID, Item.Key.SID);
+                sb2.AppendFormat("<div class=\"list\" ts=\"{0}\" on=\"{1}\" s=\"{2}\">", Item.Key.TSID, Item.Key.ONID, Item.Key.SID);
                 if (Item.Value == null || Item.Value.Count == 0)
                 {
                     if (!IncludeNotEpg) continue;
@@ -168,8 +173,6 @@ namespace EpgTimer
                         //Debug.Print("EPG Header {0}", Item.Key.service_name);
                         if (Event.Short == null || !Event.StartFlg) continue;
                         DateTime EventStart = Event.Start, EventEnd = Event.End;
-                        EventStart = EventStart.AddSeconds(-1 * EventStart.Second); // 19:00:03 => 19:00:00
-                        EventEnd = EventEnd.AddSeconds(-1 * EventEnd.Second);
                         long Size = 0;
                         OldTime = EventEnd;
                         if (EventStart < Start)
@@ -183,17 +186,14 @@ namespace EpgTimer
                         var EventName = String.Format("{0} <span title=\"{1}\">{1}</span>\n<p>{2}</p>\n", StartTimeStr,
                             HttpUtility.HtmlEncode(Event.Short.event_name),
                             HttpUtility.HtmlEncode(Event.Short.text_char));
-                        DateTime Time1 = EventStart;
+                        DateTime Time1 = ConvertDateTime(EventStart);
                         long Top = 0;
-                        if (UnixTime.ToUnixTime(Time1) % 3600 != 0)
-                            Time1 = Time1.AddSeconds(-1 * (UnixTime.ToUnixTime(Time1) % 3600));
                         foreach (var Time2 in TimeList.Values)
                         {
-                            if (Time1 == Time2) break;
+                            if (Time1 == ConvertDateTime(Time2)) break;
                             Top += MinSize * 60;
                         }
                         Top += ((UnixTime.ToUnixTime(EventStart) - UnixTime.ToUnixTime(Start)) % 3600) / 60 * MinSize;
-                        //long Top = (UnixTime.ToUnixTime(EventStart) - UnixTime.ToUnixTime(Start)) / 60 * MinSize;
                         var Reserve = CommonManager.Instance.DB.ReserveList.Values.Where(s =>
                             s.EventID == Event.EID &&
                             s.ServiceID == Event.SID &&
@@ -205,16 +205,14 @@ namespace EpgTimer
                             if (Event.Content != null && Event.Content.nibbleList != null && Event.Content.nibbleList.Count != 0 &&
                                 Setting.Instance.ContentToColorTable
                                         .Count(s => s.ContentLevel1 == Event.Content.nibbleList[0].content_nibble_level_1) > 0)
-                                sb2.AppendFormat("<div class=\"event{5}\" data-e=\"{0}\" style=\"background:{2};top:{4}px;min-height:{3}px;max-height:{3}px;z-index:{6};\">{1}</div>\n", Event.EID, EventName,
-                                    Setting.Instance.ContentToColorTable
-                                        .Where(s => s.ContentLevel1 == Event.Content.nibbleList[0].content_nibble_level_1).First().Color, Size, Top, AddClass, ItemCount);
+                                sb2.AppendFormat("<div class=\"event{4} ct-{6}\" e=\"{0}\" style=\"top:{3}px;min-height:{2}px;max-height:{2}px;z-index:{5};\">{1}</div>\n", Event.EID, EventName, Size, Top, AddClass, ItemCount, Event.Content.nibbleList[0].content_nibble_level_1);
                             else
-                                sb2.AppendFormat("<div class=\"event{4}\" data-e=\"{0}\" style=\"top:{3}px;min-height:{2}px;max-height:{2}px;z-index:{5};\">{1}</div>\n",
+                                sb2.AppendFormat("<div class=\"event{4}\" e=\"{0}\" style=\"top:{3}px;min-height:{2}px;max-height:{2}px;z-index:{5};\">{1}</div>\n",
                                     Event.EID, EventName, Size, Top, AddClass, ItemCount);
                         }
                         else
                         {
-                            sb2.AppendFormat("<div class=\"event{4}\" data-e=\"{0}\" style=\"top:{3}px;min-height:{2}px;max-height:{2}px;z-index:{5};\">{1}</div>\n",
+                            sb2.AppendFormat("<div class=\"event{4}\" e=\"{0}\" style=\"top:{3}px;min-height:{2}px;max-height:{2}px;z-index:{5};\">{1}</div>\n",
                                 Event.EID, EventName, Size, Top, AddClass, ItemCount);
                         }
                         ItemCount++;
